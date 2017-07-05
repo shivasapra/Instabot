9f1bfb9901e3a65d9b1eced7d50bd2e1c7661b6e
shiva <ashishsapra7@gmail.com>
2017-07-05 02:36:27 -0700

    self_info file is added which contain a function to get our own info

---
+++ self_info.py
@@ -0,0 +1,19 @@
+from constants import Access_Token, BASE_URL
+import requests
+
+
+# a function to get own info
+def self_info():
+    request_url = BASE_URL + 'users/self/?access_token=%s' % Access_Token
+    print 'GET request url : %s' % request_url
+    user_info = requests.get(request_url).json()
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']) > 0:
+            print 'username : ' + user_info['data']['username']
+            print 'no. of followers : ' + user_info['data']['counts']['followed_by']
+            print 'no. of followings : ' + user_info['data']['counts']['follows']
+            print 'no. of posts : ' + user_info['data']['counts']['media']
+        else:
+            print 'user does not exist'
+    else:
+        print 'request not completed'
