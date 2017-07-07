3a1487461a27c7929e815fe73b8ea802d9197eea
shiva <ashishsapra7@gmail.com>
2017-07-06 22:42:27 -0700

    get_post_id function added to fetch id of recent post of a user

---
+++ get_post_id.py
@@ -0,0 +1,21 @@
+from constants import Access_Token, BASE_URL
+import requests
+from get_user_id import get_user_id
+
+
+# function is defined to get another user's post
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'user does not exist'
+        exit()
+    request_url = BASE_URL + 'users/%s/media/recent/?access_token=%s' % (user_id, Access_Token)
+    print 'GET request_url : ' + request_url
+    user_media = requests.get(request_url).json()
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']) > 0:
+            return user_media
+        else:
+            print 'there is no recent post'
+    else:
+        print 'request not completed'
