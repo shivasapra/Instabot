6a444dc7d01b9c53e9edc592d7b3252e400be78e
shiva <ashishsapra7@gmail.com>
2017-07-06 23:14:11 -0700

    post_a_comment function is added to comment on recent post ofa user

---
+++ post_a_comment.py
@@ -0,0 +1,16 @@
+import request
+from constants import BASE_URL,Access_Token
+from get_post_id import get_post_id
+
+
+# function is defined to like a recent post of a user
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    comment = raw_input('enter comment')
+    request_url= BASE_URL + 'media/%s/comments' % media_id
+    payload = {"access_token": Access_Token, "message": comment}
+    post_a_comment = request.post(request_url,payload).json()
+    if post_a_comment['meta']['data'] == 200:
+        print "post commented successfully"
+    else:
+        print "unable to post a comment"
