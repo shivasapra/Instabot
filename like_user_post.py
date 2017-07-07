0bb0e19f8b79f5ecce5096a7e53c2807de3c5972
shiva <ashishsapra7@gmail.com>
2017-07-06 22:44:49 -0700

    like user post function added to like a recent post of a user

---
+++ like_user_post.py
@@ -0,0 +1,15 @@
+import request
+from constants import BASE_URL,Access_Token
+from  get_post_id import get_post_id
+
+
+# function is defined to like a recent post of a user
+def like_user_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url= BASE_URL + 'media/%s/likes' % media_id
+    payload = {"access_token": Access_Token}
+    like_a_post = request.post(request_url,payload).json()
+    if like_a_post['meta']['data'] == 200:
+        print "post liked successfully"
+    else:
+        print "unable to like post"
